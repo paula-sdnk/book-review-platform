ff7584c66b70ebc0107c6472f91c36687cc8cdc1
Paula Sadenok <paula.saden@gmail.com>
2026-03-23 17:17:45 +0200

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,21 @@
+import { db } from "@book-review-platform/db";
+import * as schema from "@book-review-platform/db/schema/auth";
+import { env } from "@book-review-platform/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  secret: env.BETTER_AUTH_SECRET,
+  baseURL: env.BETTER_AUTH_URL,
+  plugins: [nextCookies()],
+});
